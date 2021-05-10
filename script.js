e9a0db85b72d99586e88e9a4f28f92108ca82326
Spideyhacker <81465649+Spideyhacker@users.noreply.github.com>
2021-05-10 23:50:17 +0530

    Improved UI

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const recipeclosebutton = document.getElementById('recipe-close-button')
 
 // event listeners
 searchbtn.addEventListener('click',getMeallist);
+mealList.addEventListener('click', getMealRecipe);
 
 
 // get meal list that matches with the ingredients
@@ -20,7 +21,7 @@ function getMeallist(){
                     <div class="Meal-item" data-id="${meal.idMeal}" style="border-radius: 2rem; overflow: hidden;background-color: white;">
                         <div class="meal-img" >
                             <img src="${meal.strMealThumb}" alt="food" style="height: 300px;
-                            width: 405px;">
+                            width: 500px;">
                         </div>    
                         <div class="meal-name">
                             <h3>${meal.strMeal}</h3>
@@ -29,13 +30,25 @@ function getMeallist(){
                     </div>   
                 `;
             });
+            mealList.classList.remove('notfound');
         } else{
-            html="Sorry,we didn't find any meal!!!";
+            html="Sorry,we didn't find any meal!";
+            mealList.classList.add('notfound');
         }
-
         mealList.innerHTML = html;
-        document.getElementById("mealListHeader").style.display = "block"
-        
     });
 
+}
+
+// get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-button')){
+        let mealItem= e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data);
+        })
+    }
 }
